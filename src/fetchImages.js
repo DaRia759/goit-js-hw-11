758d2127916cdc9cc4436a9ef4eb2d8815bbc6b5
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-05-02 18:37:19 +0200

    key

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -5,7 +5,7 @@ export default async function fetchImages(inputRequest, page, per_page) {
 
     const KEY = "35927005-0d9b5dfd4ead8e82c902493f1";
     const BASE_URL = "https://pixabay.com/api";
-    const filters = `key=${KEY}&q=${inputRequest}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const filters = `?key=${KEY}&q=${inputRequest}&image_type=photo&orientation=horizontal&safesearch=true`;
     const URL = `${BASE_URL}/?filters=${filters}&page=${page}&per_page=${per_page}`;
     
     const response = await axios.get(URL);
